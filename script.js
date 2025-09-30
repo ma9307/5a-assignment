0d73a2b68ed2a37ee28b0442212bd8f051f79b6a
ma9307 <ma9307@nyu.edu>
2025-09-30 09:50:55 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let insideOfBridge = document.getElementById("inside");
+
+insideOfBridge.addEventListener("click", () => {
+  document.getElementById("textbox").innerText =
+    "You clicked on the inside of the Jade Belt Bridge in Beijing!";
+});
+
+
